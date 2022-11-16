a95e5d011c93cb687bcb996fdfbb95de5798a336
杨一凡 <1207835084@qq.com>
2022-11-16 19:07:06 +0800

    新增

--- src/appview/UploadProjectView.vue
+++ src/appview/UploadProjectView.vue
@@ -1,9 +1,16 @@
 <template>
   <back msg="上传产品" fromWhere="/AppHome"></back>
   <el-card v-for="(item,i) in myProject" class="project" :class="item.num===1?'gray':'green'">
-
+  {{item.name}}
+  </el-card>
+  <el-card class="project">
+    <div>
+      <van-icon name="add-o" style="left: 35%;font-size: 20px" />
+      <span style="position:absolute;left: 45%;font-size: 20px">新增</span>
+    </div>
   </el-card>
 </template>
+
 
 <script>
 import back from "@/components/back";
@@ -14,9 +21,10 @@ export default {
   },data(){
     return{
       myProject:[{
+        name:'西瓜',
         num:1
       },{
-        num:2
+        name:'白菜'
       }],
     }
   }
